81024284c38734ba0d276e9988224ed1f97c571a
matadorif <54037350+matadorif@users.noreply.github.com>
2019-09-23 15:27:41 -0300

    Update ex10.c

--- ex10.c
+++ ex10.c
@@ -15,16 +15,14 @@ int main(){
 	}
 	
 	for(i = 0; i < 3; i++){
-		for(j = 0; j < 3; j++){
-			if(m[i][j] == m[i+1]){
-				printf("Linha %d tem valores iguais. Valor da linha: %d\n", i+1, m[i][j]);
-				break;
-			}
-			if(m[i][j] == m[i][j+1]){
-				printf("Coluna %d tem valores iguais. Valor da coluna: %d\n", j+1, m[i][j]);
-				break;
-			}
+		if(m[i][0] == m[i][1] && m[i][0] == m[i][2]){
+			printf("Linha %d tem valores iguais. Valor da linha: %d\n", i+1, m[i][0]);
 		}
+		
+		if(m[0][i] == m[1][i] && m[0][i] == m[2][i]){
+			printf("Coluna %d tem valores iguais. Valor da coluna: %d\n", i+1, m[0][j]);
+		}
+		
 	}
 	
 	
